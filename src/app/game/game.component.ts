d62b761bf7056b77260f3f1de32c7ddc47587744
GottfriedOswald <go.oswald@t-online.de>
2022-03-07 22:47:48 +0100

    add animation when pick a card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-
+  cardGrabbed = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  //checks if game card is clicked
+  takeCard() {
+    if (this.cardGrabbed == true) {
+      this.cardGrabbed = false;
+    } else {
+      this.cardGrabbed = true;
+    }
+  }
 }
