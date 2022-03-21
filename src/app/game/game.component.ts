52e28f759ae2591a2b8265a78e0e50f8f4cb1800
GottfriedOswald <go.oswald@t-online.de>
2022-03-21 12:38:10 +0100

    push cards in stack-Array and shuffle it

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   cardGrabbed = false;
+  game: Game;
   constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game);
   }
 
   //checks if game card is clicked
