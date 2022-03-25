a7e4686e5edaa2c4fcffa0d8e35e7360858d1ebf
GottfriedOswald <go.oswald@t-online.de>
2022-03-25 10:56:51 +0100

    show different Cards by click on stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,23 +9,70 @@ import { Game } from 'src/models/game';
 export class GameComponent implements OnInit {
   cardGrabbed = false;
   game: Game;
+
+  //eine Variable vom Typ string erstellen die eine Spielkarte aus dem Array (stack) speichert.
+  currendCard: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
     this.newGame();
   }
 
-  newGame(){
-    this.game = new Game();
+  gameLogConsole() {
     console.log(this.game);
+  };
+
+  newGame() {
+    this.game = new Game();
+    this.gameLogConsole();
   }
 
   //checks if game card is clicked
   takeCard() {
     if (this.cardGrabbed == true) {
-      this.cardGrabbed = false;
+      this.setGrabbedCardAsFalse();
     } else {
-      this.cardGrabbed = true;
+      this.setGrabbedCardAsTrue();
+      this.setCurrendCard();
+      this.setCurrendCardToPlayedCards();
+      this.gameLogConsole();
+      setTimeout(() => {
+        this.setGrabbedCardAsFalse();
+      }, 1000);
+    }
+  }
+
+  setGrabbedCardAsFalse() {
+    this.cardGrabbed = false;
+  }
+
+  setGrabbedCardAsTrue() {
+    this.cardGrabbed = true;
+  }
+
+  setCurrendCard() {
+    if (!this.checkAmountCardsInStack) {
+      this.currendCard = undefined;
+    } else {
+      this.currendCard = this.game.stack.pop();
+    }
+  }
+
+  setCurrendCardToPlayedCards() {
+    if (this.currendCard !== undefined) {
+      this.game.playedCards.push(this.currendCard);
+    } else {
+      alert('Sorry, no more Cards in Stack!');
+    }
+  }
+
+  checkAmountCardsInStack() {
+    let amount = this.game.stack.length;
+    if (amount < 1) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
